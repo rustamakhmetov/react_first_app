30a820fb4186cf44b192451142350f5bdc84c064
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-07-10 22:41:38 +0500

    add first react app

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import Add, * as math from "./src/math";
+import React from 'react';
+import ReactDOM from "react-dom";
 
-console.log(`add(5,4): ${Add(5, 4)}`);
-console.log(`sub(5,4): ${math.sub(5, 4)}`);
-console.log(`div(5,4): ${math.div(5, 4)}`);
-console.log(`mul(5,4): ${math.mul(5, 4)}`);
+import Hello from "./src/Hello";
+
+ReactDOM.render(
+    <Hello />,
+    document.getElementById("root")
+);
--- src/math.js
+++
@@ -1,6 +0,0 @@
-const add = (a, b) => a + b;
-export const sub = (a, b) => a - b;
-export const div = (a, b) => a / b;
-export const mul = (a, b) => a * b
-
-export default add;
